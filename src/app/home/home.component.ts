9b5777e27e0156b071b67a85ba1433a372971923
GauravSase2207 <gauravsase1115@gmail.com>
2025-01-04 12:59:56 +0530

    full upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { product } from '../data-type';
-import { ProductService } from '../services/product.service';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
- popularProducts:undefined|product[];
- trendyProducts:undefined | product[];
-  constructor(private product:ProductService) {}
+export class HomeComponent {
 
-  ngOnInit(): void {
-    this.product.popularProducts().subscribe((data)=>{
-      this.popularProducts=data;
-    })
-
-    this.product.trendyProducts().subscribe((data)=>{
-      this.trendyProducts=data;
-    })
-  }
 }
